7ea219ddec674f666920b6a9278b1ed0b8df87f3
neli <neli@DESKTOP-F1HSTJ7>
2021-04-05 09:16:21 -0300

    Alteração do cep e inclusão de novo objeto

--- Relacionamentos/src/TestaCliente.java
+++ Relacionamentos/src/TestaCliente.java
@@ -9,12 +9,15 @@ public class TestaCliente {
 		//Endereco e = new Endereco();//não precisa criar o endereco pois já nasceu com o cliente
 		c.getEnd().setLogradouro("Rua XV");
 		c.getEnd().setNumero(135);
-		c.getEnd().setCep("80.000-000");
+		c.getEnd().setCep("85.555-666");
 		
 		//não precisa mais ligar os objetos
 		//c.setEnd(e);
 		
 		c.mostra();
+		Cliente c2 = new Cliente();
+		c2.setNome("Armenegildo Zegna");
+		c2.mostra();
 		
 	}
 
